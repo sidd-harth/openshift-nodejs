aa1e85072585b7482012006528f494861c57d69a
Siddharth Barahalikar <barahalikar.siddharth@gmail.com>
2018-01-21 00:34:32 +0530

    r

--- config/database.config.js
+++ config/database.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     //url: 'mongodb://localhost:27017/easy-notes'
 	//url: 'mongodb://admin:admin@mongodb/admin'
-	url: 'mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@mongodb/${MONGODB_DATABASE}'
+	url: 'mongodb://${mongoUser}:${mongoPass}@mongodb/${mongoDb}'
 }
