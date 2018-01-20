6d07022d364f1f534e0a4f370a733164d347a7fa
Siddharth Barahalikar <barahalikar.siddharth@gmail.com>
2018-01-21 00:40:21 +0530

    3

--- config/database.config.js
+++ config/database.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     //url: 'mongodb://localhost:27017/easy-notes'
-	//url: 'mongodb://admin:admin@mongodb/admin'
-	url: 'mongodb://${mongoUser}:${mongoPass}@mongodb/admin'
+	url: 'mongodb://admin:admin@mongodb/admin'
+	//url: 'mongodb://${mongoUser}:${mongoPass}@mongodb/admin'
 }
