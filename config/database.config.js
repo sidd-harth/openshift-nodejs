0474376c592d5dcef03d25a5442706d44e7dcb79
Siddharth Barahalikar <barahalikar.siddharth@gmail.com>
2018-01-21 00:31:15 +0530

    mongodb env variables

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     //url: 'mongodb://localhost:27017/easy-notes'
-	url: 'mongodb://admin:admin@mongodb/admin'
+	//url: 'mongodb://admin:admin@mongodb/admin'
+	url: 'mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@mongodb/${MONGODB_DATABASE}'
 }
